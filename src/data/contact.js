25a017916d87443430ec1c36276bba96f2c9a507
valentindeugenio <deugeniovalentin@gmail.com>
2026-07-06 01:23:27 -0300

    Mejoras en la landing odontológica

--- src/data/contact.js
+++ src/data/contact.js
@@ -6,12 +6,6 @@ export const contactOptions = [
     featured: false
   },
   {
-    title: 'Swiss Medical',
-    detail: 'Consultar disponibilidad',
-    link: 'https://wa.me/5491133932597?text=Hola.%20Quisiera%20agendar%20una%20consulta%20odontol%C3%B3gica.%20Tengo%20cobertura%20Swiss%20Medical.',
-    featured: false
-  },
-  {
     title: 'Particular',
     detail: 'Atención personalizada',
     link: 'https://wa.me/5491133932597?text=Hola.%20Quisiera%20agendar%20una%20consulta%20odontol%C3%B3gica%20de%20forma%20particular.',
